42da32550c11755c2cd5cb2ff8e548165b932958
Thomas BOIDUN <thomas.boidun@live.fr>
2022-06-26 17:49:56 +0200

    initialization of express.js

--- app.js
+++ app.js
@@ -2,5 +2,15 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res, next) {
-    res.send('hello world');
+    res.send('Hello World!');
 })
+
+const port = 3000;
+
+const run = () => {
+    const date = new Date();
+    const mess = `⚡ Server running: ${date.toLocaleDateString()} ${date.toLocaleTimeString()} ⚡` 
+    console.log(mess);
+};
+
+app.listen(port, run());
